d77b3d44b51fe3c1eaab2b3e915b436e0e279a19
Christopher Brito <cbrito@gmail.com>
2012-04-15 19:19:13 -0400

    Updated chartSpecificOptions for lineGraph
    Switched from << to += to build the string.

--- GoogleImageCharts/line-graph.rb
+++ GoogleImageCharts/line-graph.rb
@@ -7,7 +7,6 @@ module GoogleImageCharts
       
       @chartType = "lc" #Line chart
       
-      @chartSpecificOptions << "&chds=a"      # Chart Scale (automatic scaling by Google)
       @chartSpecificOptions << "&chxt=x,y"    # Scale labels for the X and Y axis'
       
     end
